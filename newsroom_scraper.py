9d8a79d6a0460d25b419895acbef535f115ef88d
Parth Gupta <parthgupta023@gmail.com>
2022-05-09 18:58:51 -0400

    Added initial script

---
+++ newsroom_scraper.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+URL = "https://www.mcgill.ca/newsroom/aggregator/categories/2"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+feed_items = soup.find_all('div', class_='feed-item')
+
+for feed in feed_items:
+    title_item = feed.find('h3', class_='feed-item-title')
+    title = title_item.find('a').text
+    print(title)
