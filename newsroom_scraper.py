a51e6b8a88f63879ac5584d999e74feafe3dd997
Parth Gupta <parthgupta023@gmail.com>
2022-05-09 19:02:43 -0400

    Changed script

--- newsroom_scraper.py
+++ newsroom_scraper.py
@@ -11,5 +11,4 @@ feed_items = soup.find_all('div', class_='feed-item')
 
 for feed in feed_items:
     title_item = feed.find('h3', class_='feed-item-title')
-    title = title_item.find('a').text
-    print(title)
+    title = title_item.find('a').text       # Get the title of the news item
