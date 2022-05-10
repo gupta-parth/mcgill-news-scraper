1dc26878ebcfdb3a5db546ff3007cd167a7b8447
Parth Gupta <parthgupta023@gmail.com>
2022-05-09 21:06:53 -0400

    Scraped over 8 pages instead of just one

--- newsroom_scraper.py
+++ newsroom_scraper.py
@@ -1,22 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
+# There are currently 8 pages in the newsroom
+for i in range(0, 9):
+    url = "https://www.mcgill.ca/newsroom/aggregator/categories/2?page="+str(i)
+    print(url)
+    page = requests.get(url)
 
-URL = "https://www.mcgill.ca/newsroom/aggregator/categories/2"
-page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    feed_items = soup.find_all('div', class_='feed-item')
 
-feed_items = soup.find_all('div', class_='feed-item')
+    for feed in feed_items:
+        title_item = feed.find('h3', class_='feed-item-title')
+        # Get the title of the news item
+        news_title = title_item.find('a').text
 
-for feed in feed_items:
-    title_item = feed.find('h3', class_='feed-item-title')
-    # Get the title of the news item
-    news_title = title_item.find('a').text
+        # Get the news item content and clean it a bit
+        news_content = feed.find(
+            'div', class_='feed-item-body').text.replace('\n', '')
 
-    # Get the news item content and clean it a bit
-    news_content = feed.find(
-        'div', class_='feed-item-body').text.replace('\n', '')
+        # Get the news date
+        news_date = feed.find('span', class_='feed-item-date').text
 
-    # Get the news date
-    news_date = feed.find('span', class_='feed-item-date').text
+        print(news_title)
