4629c50153b4acc7ee03e0d9ddb11884a751e29c
Parth Gupta <parthgupta023@gmail.com>
2022-05-09 20:59:29 -0400

    Scrape the content and make categories

--- newsroom_scraper.py
+++ newsroom_scraper.py
@@ -11,4 +11,12 @@ feed_items = soup.find_all('div', class_='feed-item')
 
 for feed in feed_items:
     title_item = feed.find('h3', class_='feed-item-title')
-    title = title_item.find('a').text       # Get the title of the news item
+    # Get the title of the news item
+    news_title = title_item.find('a').text
+
+    # Get the news item content and clean it a bit
+    news_content = feed.find(
+        'div', class_='feed-item-body').text.replace('\n', '')
+
+    # Get the news date
+    news_date = feed.find('span', class_='feed-item-date').text
