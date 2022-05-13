ec3a6e789c16af2060b7e424d8a5b08f7bef3970
Parth Gupta <parthgupta023@gmail.com>
2022-05-13 17:23:47 -0400

    Added scraping the full news content for Campus News

--- scripts/campus_news.py
+++ scripts/campus_news.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from csv import writer
-
+import re
 
 with open('data/campus_news_data.csv', 'w', encoding='utf8', newline='') as f:
     # CSV writer init
@@ -23,13 +23,24 @@ with open('data/campus_news_data.csv', 'w', encoding='utf8', newline='') as f:
             title_item = feed.find('h3', class_='feed-item-title')
             # Get the title of the news item
             news_title = title_item.find('a').text
-
+            '''
             # Get the news item content and clean it a bit
             news_summary = feed.find(
                 'div', class_='feed-item-body').text.replace('\n', '').strip()
+            '''
 
             # Get the news date
             news_date = feed.find('span', class_='feed-item-date').text
 
-            info = [news_title, news_summary, news_date]
+            # Get the news content
+            news_link = title_item.find('a')['href']
+            news_page = requests.get(news_link)
+            # Create news soup object
+            news_soup = BeautifulSoup(news_page.content, 'html.parser')
+
+            news_content = news_soup.find(
+                'div', class_=re.compile('entry-content|article-content'))
+            all_content = news_content.get_text().replace('\n', '').strip()
+
+            info = [news_title, all_content, news_date]
             out.writerow(info)
