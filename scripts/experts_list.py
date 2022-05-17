d7250c8bcffe4401cbda41e5dd586d7b00afe8fc
Parth Gupta <parthgupta023@gmail.com>
2022-05-16 23:13:24 -0400

    Renamed file and scraped

---
+++ scripts/experts_list.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+import re
+
+
+for i in range(0, 43):
+    url = "https://www.mcgill.ca/newsroom/people/field_mprofile_guide_subjects/Human%20Resources%20/%20Organizational%20Behaviour/field_mprofile_department/Faculty%20of%20Medicine?page=" + \
+        str(i)
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    profiles = soup.find_all(
+        'div', class_=re.compile('profiles-glossary-entry'))
+
+    for profile in profiles:
+        name = profile.find('h2', class_='mcgill-profiles-display-name').text
+        title_div = profile.find(
+            'div', class_='views-field views-field-field-mprofile-title')
+        try:
+            title = title_div.find('span', class_='field-content').text
+        except:
+            title = ''
+        department_div = profile.find(
+            'div', class_='views-field views-field-field-mprofile-department')
+        try:
+            department = department_div.find(
+                'span', class_='field-content').text
+        except:
+            department = ''
+        print(i)
+        print(name)
+        print(title)
+        print(department)
+        print()
